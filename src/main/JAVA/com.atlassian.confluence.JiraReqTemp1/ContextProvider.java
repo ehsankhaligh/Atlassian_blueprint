97f05b0ab420b820e1bcb30465eb4814746054ae
Khaligh. Ehsan <KhalighE@saccounty.net>
2016-11-06 07:57:37 -0800

    commit 6

---
+++ src/main/JAVA/com.atlassian.confluence.JiraReqTemp1/ContextProvider.java
@@ -0,0 +1,27 @@
+//package com.example.plugins.tutorial.confluence.simplebp;
+package com.plugins.confluence.JiraReqTemp1;
+
+import com.atlassian.confluence.plugins.createcontent.api.contextproviders.AbstractBlueprintContextProvider;
+import com.atlassian.confluence.xhtml.api.EditorFormatService;
+import com.atlassian.confluence.xhtml.api.XhtmlContent;
+import com.atlassian.confluence.plugins.createcontent.api.contextproviders.BlueprintContext;
+
+/*
+public class MyContextProvider extends AbstractBlueprintContextProvider {
+	
+	
+	@Override
+    protected BlueprintContext updateBlueprintContext(BlueprintContext blueprintContext)
+    {
+		String appName = blueprintContext.get("vAppName").toString();
+		//blueprintContext.put("vAppName",vAppName);
+		//blueprintContext.put("vSupportTeam",vSupportTeam);
+		
+		String contentPageTitle = appName + " - Application Specification";
+		
+        blueprintContext.setTitle(contentPageTitle);
+		
+        return blueprintContext;
+    }
+
+}*/
